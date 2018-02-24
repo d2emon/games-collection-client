901378fca2a3da5244aea69c144ef5e5bc2ffc77
Dmitry Kutsenko <d2emonium@gmail.com>
2018-02-24 09:55:24 +0300

    Refactoring main file

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store/server'
+// import Vuex from 'vuex' // ?
 import Vuetify from 'vuetify'
+// import BootstrapVue from 'bootstrap-vue'
+// import VueCookie from 'vue-cookie'
+import router from './router'
+import store from './store'
 // import 'vuetify/dist/vuetify.css'
+import App from './App'
+
+// import 'vue-material-design-icons/styles.css'
+// import 'mdi/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
+// Vue.use(Vuex)
 Vue.use(Vuetify)
-
-// import store from './store/server'
+// Vue.use(VueCookie)
+// Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
